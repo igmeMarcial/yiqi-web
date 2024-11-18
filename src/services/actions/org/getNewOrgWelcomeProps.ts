82fded0517d65736fa6e323f29f6fb343ecf9bd2
Paul Garcia <pbrain19@gmail.com>
2024-11-18 16:24:15 -0500

    adjustments

--- src/services/actions/org/getNewOrgWelcomeProps.ts
+++ src/services/actions/org/getNewOrgWelcomeProps.ts
@@ -3,27 +3,26 @@
 import prisma from '@/lib/prisma'
 
 export async function getNewOrgWelcomeProps(organizationId: string) {
-  const [isStripeSetup, contacts, hasEvents, hasNotifications] =
-    await Promise.all([
-      prisma.organization.findUnique({
-        where: { id: organizationId },
-        select: { stripeAccountId: true }
-      }),
-      prisma.organizationContact.findFirst({
-        where: { organizationId }
-      }),
-      prisma.event.findFirst({
-        where: { organizationId }
-      }),
-      prisma.notification.findFirst({
-        where: { organizationId }
-      })
-    ])
+  const [org, contacts, hasEvents, hasNotifications] = await Promise.all([
+    prisma.organization.findUnique({
+      where: { id: organizationId },
+      select: { stripeAccountId: true }
+    }),
+    prisma.organizationContact.findFirst({
+      where: { organizationId }
+    }),
+    prisma.event.findFirst({
+      where: { organizationId }
+    }),
+    prisma.notification.findFirst({
+      where: { organizationId }
+    })
+  ])
 
   return {
     hasContacts: !!contacts,
     hasEvents: !!hasEvents,
     hasNotifications: !!hasNotifications,
-    isStripeSetup: !!isStripeSetup
+    isStripeSetup: !!org?.stripeAccountId
   }
 }
