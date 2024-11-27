10fb962aa73e7ce1b54ad42c8a583d903c26e4ca
Paul Garcia <pbrain19@gmail.com>
2024-11-27 11:39:51 -0500

    adjusting some small stuff

--- prisma/migrations/20241127163915_event_extras/migration.sql
+++ prisma/migrations/20241127163915_event_extras/migration.sql
@@ -1,8 +1,6 @@
 -- AlterTable
 ALTER TABLE "Event" ADD COLUMN     "backgroundColor" TEXT,
-ADD COLUMN     "endTime" TEXT,
 ADD COLUMN     "featuredIn" JSONB,
 ADD COLUMN     "heroImage" TEXT,
 ADD COLUMN     "hosts" JSONB,
-ADD COLUMN     "startTime" TEXT,
 ADD COLUMN     "subtitle" TEXT;
