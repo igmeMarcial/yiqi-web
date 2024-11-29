5e5033802265f03866cd2282720d9fc493301ca4
Daniel151505 <daniel150520011@gmail.com>
2024-11-29 02:16:32 -0500

    feat: Removed close dialog

--- src/components/tickets/qrModal/QrModal.tsx
+++ src/components/tickets/qrModal/QrModal.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogClose } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { QRCodeSVG } from "qrcode.react";
 
 interface QRModalProps {
