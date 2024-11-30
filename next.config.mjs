5aaa15414f3bb5c08099229fe33e10c71fee0128
Edartruwu <edartru.contactos@gmail.com>
2024-11-29 22:13:38 -0500

    pre deploy fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
+/** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin()
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
   images: {
