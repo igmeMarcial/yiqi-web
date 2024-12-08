8d9dc76a71112c9aa07443f850a949571aeeffb1
anamaria.zarate <anamaria.zarate@rainfocus.com>
2024-12-08 00:10:02 -0500

    Improve view for organizer

--- src/app/(protected)/admin/organizations/[id]/organizers/page.tsx
+++ src/app/(protected)/admin/organizations/[id]/organizers/page.tsx
@@ -1,10 +1,9 @@
 import { getOrganization } from '@/services/actions/organizationActions'
 import { getOrganizersByOrganization } from '@/services/actions/organizerActions'
-import Link from 'next/link'
 import AddOrganizerButton from './AddOrganizerButton'
 import OrganizationLayout from '@/components/orgs/OrganizationLayout'
 import { getUser } from '@/lib/auth/lucia'
-import { getLocale, getTranslations } from 'next-intl/server'
+import { getTranslations } from 'next-intl/server'
 
 export default async function OrganizersPage({
   params
@@ -15,42 +14,61 @@ export default async function OrganizersPage({
   const organization = await getOrganization(params.id)
   const organizers = await getOrganizersByOrganization(params.id)
 
-  const t = await getTranslations('contactFor')
-  const localActive = await getLocale()
+  const t = await getTranslations('DeleteAccount')
 
   if (!organization || !user) {
     return <div>{t('organizationNotFound')}</div>
   }
 
   return (
-    <OrganizationLayout
-      orgId={params.id}
-      userProps={{
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        picture: user.picture ?? ''
-      }}
-    >
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4">
-          {t('manageOrganizersFor')} {organization.name}
-        </h1>
-        <ul className="space-y-2">
-          {organizers.map(organizer => (
-            <li key={organizer.id} className="border p-2 rounded">
-              {organizer.user.name} - {organizer.role}
-            </li>
-          ))}
-        </ul>
-        <AddOrganizerButton organizationId={params.id} />
-        <Link
-          href={`/${localActive}/admin/organizations/${params.id}`}
-          className="mt-4 inline-block text-blue-500 hover:underline"
-        >
-          {t('backToOrganizationDashboard')}
-        </Link>
-      </div>
-    </OrganizationLayout>
+    <main className="flex flex-col items-center justify-center">
+      <OrganizationLayout
+        orgId={params.id}
+        userProps={{
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          picture: user.picture ?? ''
+        }}
+      >
+        <section className="w-full h-screen p-4 rounded-lg border text-card-foreground shadow-sm bg-primary">
+          <div className="flex w-full justify-between">
+            <h1 className="text-xl sm:text-2xl font-bold">
+              {t('manageOrganizersFor') + ' '} {organization.name}
+            </h1>
+            <div>
+              <AddOrganizerButton organizationId={params.id} />
+            </div>
+          </div>
+
+          {/* Encabezado para la tabla */}
+          <div className="flex flex-row justify-between bg-gray-700 p-4 rounded mt-4">
+            <div className="col-span-6 text-sm font-semibold text-primary">
+              {t('organizer')}
+            </div>
+            <div className="col-span-3 text-sm font-semibold text-primary"></div>
+            <div className="col-span-3 text-sm font-semibold text-primary text-right">
+              {t('rol')}
+            </div>
+          </div>
+
+          <div className="overflow-y-auto dark:bg-primary rounded-lg">
+            {organizers.map(organizer => (
+              <div
+                key={organizer.user.id}
+                className="flex flex-row justify-between border-b border-gray-700"
+              >
+                <div className="flex flex-row items-center last:rounded-b-md p-4 transition-all">
+                  {organizer.user.name}
+                </div>
+                <div className="flex flex-row items-center dark:bg-primary last:rounded-b-md p-4 transition-all">
+                  {organizer.role}
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </OrganizationLayout>
+    </main>
   )
 }
