c41e4899e734718c37f11c824f9e509d2329e82e
Paul Garcia <pbrain19@gmail.com>
2024-12-17 11:53:25 -0500

    adjustments to the crons

---
+++ src/app/api/cron/jobs/route.ts
@@ -0,0 +1,14 @@
+import { notificationScheduler } from '@/lib/cron/notificationScheduler'
+import { processQueueJobs } from '@/lib/cron/queueCron'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    await Promise.all([processQueueJobs(), notificationScheduler()])
+
+    return new NextResponse(null, { status: 200 })
+  } catch (e) {
+    console.error(e)
+    return new NextResponse(null, { status: 500 })
+  }
+}
