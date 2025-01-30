9a1c7b8f357aa115ee72a10de8c0821d6ba95864
cnerylozada <cnerylozada@gmail.com>
2025-01-30 16:46:45 -0500

    Refactor/convert organizationlayout to nextjslayout (#81)
    * refact: organization-layout works as nextjs layout
    
    * chore: in progress
    
    * refact: remove unnecessary organization-layout from pages
    
    * feat: remove organization-layout from chat page

--- src/app/(protected)/admin/organizations/[id]/page.tsx
+++ src/app/(protected)/admin/organizations/[id]/page.tsx
@@ -1,20 +1,11 @@
 import { getUser } from '@/lib/auth/lucia'
 import { redirect } from 'next/navigation'
 import { Roles } from '@prisma/client'
-import { getOrganization } from '@/services/actions/organizationActions'
-
-import OrganizationLayout from '@/components/orgs/OrganizationLayout'
 import WelcomeScreen from '@/components/orgs/WelcomeNewOrg'
 import RegistrationChart from '@/components/orgs/RegistrationChart'
 import { getNewOrgWelcomeProps } from '@/services/actions/org/getNewOrgWelcomeProps'
-import { getTranslations } from 'next-intl/server'
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const t = await getTranslations('contactFor')
-  const organization = await getOrganization(params.id)
-  if (!organization) {
-    return <div>{t('organizationNotFound')}</div>
-  }
   const {
     hasContacts,
     hasEvents,
@@ -25,22 +16,10 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   const user = await getUser()
 
-  if (!user) {
-    redirect(`/auth`)
-  }
-
-  if (user.role === Roles.ADMIN) {
-    return (
-      <main className="flex flex-col items-center justify-center">
-        <OrganizationLayout
-          orgId={params.id}
-          userProps={{
-            id: user.id,
-            picture: user.picture,
-            email: user.email,
-            name: user.name
-          }}
-        >
+  if (user) {
+    if (user.role === Roles.ADMIN) {
+      return (
+        <>
           {!hasFirstRegistration ? (
             <WelcomeScreen
               importedContacts={hasContacts}
@@ -54,14 +33,14 @@ export default async function Page({ params }: { params: { id: string } }) {
               <RegistrationChart organizationId={params.id} />
             </div>
           )}
-        </OrganizationLayout>
-      </main>
-    )
-  } else if (user.role === Roles.NEW_USER) {
-    redirect(`/newuser`)
-  } else if (user.role === Roles.USER) {
-    redirect(`/user`)
-  } else if (user.role === Roles.ANDINO_ADMIN) {
-    redirect(`/andino-admin`)
+        </>
+      )
+    } else if (user.role === Roles.NEW_USER) {
+      redirect(`/newuser`)
+    } else if (user.role === Roles.USER) {
+      redirect(`/user`)
+    } else if (user.role === Roles.ANDINO_ADMIN) {
+      redirect(`/andino-admin`)
+    }
   }
 }
