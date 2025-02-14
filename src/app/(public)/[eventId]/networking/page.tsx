735cd22e3d262becfe803d4d41835702e291849f
Edartruwu <edartru.contactos@gmail.com>
2025-02-13 21:27:29 -0500

    add fixxxxxxxxxxxxxxxxxxxxx

--- src/app/(public)/[eventId]/networking/page.tsx
+++ src/app/(public)/[eventId]/networking/page.tsx
@@ -2,18 +2,24 @@ import MainLandingNav from '@/components/mainLanding/mainNav'
 import { getUser } from '@/lib/auth/lucia'
 import { redirect } from 'next/navigation'
 import { MatchesList } from './_temp/tempcomp'
-import { getNetworkingMatches } from '@/services/actions/networking/getNetworkingMatches'
+import { getNetworkingMatchesByEventUser } from '@/services/actions/networking/getNetworkingMatches'
 
-export default async function Page() {
+export default async function Page({
+  params
+}: {
+  params: { eventId: string }
+}) {
+  console.log('Event ID:', params.eventId) // Log the eventId
+
   const user = await getUser()
   if (!user) {
     redirect('/auth')
   }
 
-  const matches = await getNetworkingMatches(user.id)
+  const matches = await getNetworkingMatchesByEventUser(user.id, params.eventId)
   console.warn(matches)
 
-  if (matches.map(v => v.id)) {
+  if (matches.length > 0) {
     return (
       <>
         <div className="fixed inset-0 h-screen w-screen -z-10 bg-black"></div>
@@ -29,7 +35,7 @@ export default async function Page() {
         <div className="fixed inset-0 h-screen w-screen -z-10 bg-black"></div>
         <MainLandingNav user={user!} />
         <div className="lg:max-w-[80%] max-w-[90%] mx-auto pt-20">
-          <p className="text-xl"> no hay matches para ti</p>
+          <p className="text-xl">No hay matches para ti</p>
         </div>
       </>
     )
