b6bf10078dec34b7bc4208bd5694385f3d78c044
cnerylozada <cnerylozada@gmail.com>
2025-02-19 17:16:37 -0500

    feat: showing social networks

--- src/schemas/networkingMatchSchema.ts
+++ src/schemas/networkingMatchSchema.ts
@@ -4,7 +4,14 @@ import { z } from 'zod'
 const networkingMatchUserSchema = z.object({
   id: z.string(),
   name: z.string().nullable(),
-  picture: z.string().nullable().optional()
+  picture: z.string().nullable().optional(),
+  dataCollected: z
+    .object({
+      linkedin: z.string().optional(),
+      x: z.string().optional(),
+      instagram: z.string().optional()
+    })
+    .nullable()
 })
 
 // Schema for a single networking match
