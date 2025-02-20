6e6010413f7e77d2a7921dd9db82ccf937ca518e
cnerylozada <cnerylozada@gmail.com>
2025-02-20 18:06:35 -0500

    refact: map user data-collected

--- src/services/actions/networking/commons.ts
+++ src/services/actions/networking/commons.ts
@@ -18,20 +18,26 @@ export const parseNetworkingMatches = (
       dataCollected: Prisma.JsonValue
     }
   }[]
-): NetworkingMatchesType => {
-  return matches.map(match => ({
-    id: match.id,
-    personDescription: match.personDescription,
-    matchReason: match.matchReason,
-    userId: match.userId,
-    registrationId: match.registrationId,
-    createdAt: match.createdAt,
-    updatedAt: match.updatedAt,
-    user: {
-      id: match.user.id,
-      name: match.user.name,
-      picture: match.user.picture,
-      dataCollected: match.user.dataCollected as Prisma.JsonObject | null // Ensure proper type assertion
+): NetworkingMatchesType =>
+  matches.map(match => {
+    const dataCollected = match.user.dataCollected as {
+      x?: string
+      instagram?: string
+      linkedin?: string
+    } | null
+    return {
+      id: match.id,
+      personDescription: match.personDescription,
+      matchReason: match.matchReason,
+      userId: match.userId,
+      registrationId: match.registrationId,
+      createdAt: match.createdAt,
+      updatedAt: match.updatedAt,
+      user: {
+        id: match.user.id,
+        name: match.user.name,
+        picture: match.user.picture,
+        dataCollected
+      }
     }
-  }))
-}
+  })
