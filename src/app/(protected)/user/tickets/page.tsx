1a04bb2072ea400c057be1ef31cf4cbb2f7aff1b
Paul Garcia <pbrain19@gmail.com>
2025-03-05 12:29:12 -0500

    adjustments to ticket screen

--- src/app/(protected)/user/tickets/page.tsx
+++ src/app/(protected)/user/tickets/page.tsx
@@ -1,23 +1,17 @@
 import TicketsPage from '@/components/tickets/ticketPage/TicketPage'
 import UserLayout from '@/components/user/UserLayout'
-import { getUser } from '@/lib/auth/lucia'
 import isNetworkingDataFilled from '@/lib/utils/isNetworkingDataFilled'
 import { profileDataSchema } from '@/schemas/userSchema'
 import { getTicketsWithEvents } from '@/services/actions/tickets/ticketActions'
-import { getUserProfile } from '@/services/actions/userActions'
 import React from 'react'
 import { getTranslations } from 'next-intl/server'
+import { getUserOrRedirect } from '@/lib/auth/getUserOrRedirect'
 
 export default async function Tickets() {
-  const userCurrent = await getUser()
+  const { user } = await getUserOrRedirect()
   const t = await getTranslations('Ticket')
 
-  if (!userCurrent?.id) {
-    return <div>{t('userNotFound')}</div>
-  }
-
-  const user = await getUserProfile(userCurrent.id)
-  const tickets = await getTicketsWithEvents(userCurrent.id)
+  const tickets = await getTicketsWithEvents(user.id)
 
   if (!user) {
     return <div>{t('userNotFound')}</div>
